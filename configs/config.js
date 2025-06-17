c033a091d3d4c2ec5940ff12483c43b29b8293b6
kenchen <independentchen@gmail.com>
2025-06-17 21:42:55 +0800

    Feat: divide different environments into seperated dbs

--- configs/config.js
+++ configs/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DEV_DATABASE,
     host: process.env.MYSQL_HOST,
     dialect: "mysql",
     logging: false,
@@ -10,16 +10,16 @@ module.exports = {
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_TEST_DATABASE,
     host: process.env.MYSQL_HOST,
-    dialect: "mysql",
+    dialect: "mariadb",
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_PROD_DATABASE,
     host: process.env.MYSQL_HOST,
-    dialect: "mysql",
+    dialect: "mariadb",
     logging: false,
   }
 }
