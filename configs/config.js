b85359e9ee3a4da3315f1895f233e2b5966cb3ee
kenchen <independentchen@gmail.com>
2025-06-17 08:44:10 +0800

    Feat: init the project structure (could be modified a bit in the future)

---
+++ configs/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http")
+const process = require("process")
+const app = require("express")
+const bodyParser = require("body-parser")
+const logger = require("morgan")
+const cors = require("cors")
+
+const port = process.env.PORT | 11451
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger("[:date[iso]] :method :url :status :res[content-length] - :response-time ms"))
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hi there"
+  })
+})
+
+const server = http.createServer(app)
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
