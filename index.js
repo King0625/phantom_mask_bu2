a55abcd60d1545063517342edac2015c8955437c
kenchen <independentchen@gmail.com>
2025-06-18 03:11:53 +0800

    Feat: finish all pharmacy routers

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser")
 const logger = require("morgan")
 const cors = require("cors")
 
+const pharmacyRouter = require("./routes/pharmacy")
+
 const port = process.env.PORT | 11451
 
 app.use(cors())
@@ -12,11 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger("[:date[iso]] :method :url :status :res[content-length] - :response-time ms"))
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hi there"
-  })
-})
+app.use("/pharmacies", pharmacyRouter)
 
 const server = http.createServer(app)
 server.listen(port, () => {
